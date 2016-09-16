b6dd1d33a0b684a9cd967fb2bde9678271a12d65
Stefan Kopieczek <stefankopieczek@gmail.com>
2016-09-16 21:45:00 +0100

    Minify some more stuff.

--- tinybrain.py
+++ tinybrain.py
@@ -2,7 +2,7 @@ import sys;p=sys.argv[1];
 def w(c): s=sys.stdout;s.write(str(c)+' ');s.flush()
 i,j,d,g=0,0,[0],[]
 while i<len(p):
-    switch=0
+    s=0
     x,y=p[i],d[j]
     if x=='>':d.append(0);j+=1
     elif x=='<':d=[0]+d
@@ -10,21 +10,16 @@ while i<len(p):
     elif x=='-':d[j]-=1
     elif x=='.':w(d[j])
     elif x==',':d[j]=(input('>'))
-    elif x == '[':
-        switch = 1
-    elif x == ']':
-        switch = -1
-
-
-    if switch != 0:
-        if (switch==1 and y==0) or (switch==-1 and y != 0):
+    elif x=='[':s=1
+    elif x==']':s=-1
+    if s != 0:
+        if (s==1 and y==0) or (s==-1 and y != 0):
             c=1
-            l,r = ('[',']') if switch==1 else (']','[')
+            l,r=('[',']') if s==1 else (']','[')
             while True:
-                i+=switch
-                if p[i] == l:
-                    c += 1
-                elif p[i] == r:
+                i+=s
+                if p[i]==l:c+=1
+                elif p[i]==r:
                     c -= 1
                     if c == 0: break
-    i += 1
+    i+=1
