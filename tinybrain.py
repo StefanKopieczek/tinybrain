3404d61295015921c5ed7b203d51643ab929ec67
Stefan Kopieczek <stefankopieczek@gmail.com>
2016-09-16 22:05:04 +0100

    A bit more minifying.

--- tinybrain.py
+++ tinybrain.py
@@ -1,9 +1,8 @@
-import sys;p=sys.argv[1];
+import sys;p=sys.argv[1]
 def w(c): s=sys.stdout;s.write(str(c)+' ');s.flush()
-i,j,d,g=0,0,[0],[]
+i=j=0;d=[0]
 while i<len(p):
-    s=0
-    x,y=p[i],d[j]
+    s=0;x,y=p[i],d[j]
     if x=='>':d.append(0);j+=1
     elif x=='<':d=[0]+d
     elif x=='+':d[j]+=1
@@ -14,8 +13,7 @@ while i<len(p):
     elif x==']':s=-1
     if s != 0:
         if (s==1 and y==0) or (s==-1 and y != 0):
-            c=1
-            l,r=('[',']') if s==1 else (']','[')
+            c=1;l,r=('[',']') if s==1 else (']','[')
             while True:
                 i+=s
                 if p[i]==l:c+=1
