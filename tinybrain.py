c457e5745d554e24bf7d2ba9ee8376fe60ca80af
Stefan Kopieczek <stefankopieczek@gmail.com>
2016-09-16 22:56:24 +0100

    Reduce length of jump check

--- tinybrain.py
+++ tinybrain.py
@@ -1,6 +1,6 @@
 import sys;p=sys.argv[1]
 def w(c):s=sys.stdout;s.write(str(c)+' ');s.flush()
-i=j=0;d=[0]
+i=j=0;d=[0];a=abs;b=bool
 while i<len(p):
  s=0;x,y=p[i],d[j]
  if x=='>':d.append(0);j+=1
@@ -9,13 +9,12 @@ while i<len(p):
  elif x=='.':w(d[j])
  elif x==',':d[j]=(input('>'))
  elif x in '[]':s=(1,-1)[x==']']
- if s!=0:
-  if(s==1 and y==0) or (s==-1 and y!=0):
-   c=1;l,r=('[',']') if s==1 else (']','[')
-   while 1:
-    i+=s
-    if p[i]==l:c+=1
-    elif p[i]==r:
-     c-=1
-     if c==0:break
+ if s!=0 and b(s+1)!=b(y):
+  c=1;l,r=('[',']') if s==1 else (']','[')
+  while 1:
+   i+=s
+   if p[i]==l:c+=1
+   elif p[i]==r:
+    c-=1
+    if c==0:break
  i+=1
